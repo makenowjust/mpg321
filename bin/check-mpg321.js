d6c59924c2b64f72a7caac8da0e36cffec1e64f2
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-03-10 09:45:00 +0900

    update files

--- bin/check-mpg321.js
+++ bin/check-mpg321.js
@@ -13,6 +13,7 @@ if (which('mpg321')) {
   console.error('mpg123 command found! but mpg321 package don\'t supports mpg123 command enough!');
 } else {
   console.error('please install mpg321 command for using mpg321 package.');
+  console.error('see https://github.com/MakeNowJust/mpg321');
   process.exit(1);
 }
   
